1a7d6cca88f67e3411ec25506b4a62613d7724c2
mnl-lab <147846109+mnl-lab@users.noreply.github.com>
2025-10-05 00:52:09 +0100

    ex 1 and 2

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -1,8 +1,27 @@
 package samplearrays;
 
+
+
 public class CourseNumbersArray {
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
+        int c = registeredCourses.length;
+        int[] updatedCourses = new int[c+1];
+        System.arraycopy(registeredCourses, 0, updatedCourses, 0, c);
+        int newCourse = 2006;
+        updatedCourses[c] = newCourse;
+        for(int i = 0; i< c+1; i++){
+            System.out.print(updatedCourses[i] + ", ");
+        }
+        int specificNumber = 2080;
+        for(int i = 0; i< c + 1; i++){
+            if(updatedCourses[i] == specificNumber){
+                System.out.println(specificNumber + " found");
+            }
+            else {
+                System.out.println(specificNumber + " not found");
+            }
+        }
 
     }
 }
